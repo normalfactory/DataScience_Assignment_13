bb33e442e5ae31e48a01f38b7b9e903251bb5e1a
Scott Mac McEachern <the_fixer@hotmail.com>
2019-05-15 13:39:15 -0700

    Completed the marker label for the chart

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,7 @@ May 13, 2019
 
 console.log("--> app.js: Started initialization of application");
 
+
 //- Prepare Globals
 const CHARTDIVNAME = "#scatter";
 
@@ -75,7 +76,7 @@ function createChart(sourceData){
     let chartWidth = (_chartDivWidth - chartMargin.left - chartMargin.right);
 
     //- Create SVG Container
-    let svgContainer = d3.select("body").select("#scatter").append("svg")
+    let svgContainer = d3.select("body").select(CHARTDIVNAME).append("svg")
         .attr("height", svgHeight)
         .attr("width", _chartDivWidth);
 
@@ -85,7 +86,67 @@ function createChart(sourceData){
         .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
 
-        
+    //-- Create Chart
+
+    let xColumnName = "poverty";
+    let yColumnName = "healthcare";
+
+
+    //- Prepare X
+    let xScale = d3.scaleLinear()
+        .domain([d3.min(sourceData, d => d[xColumnName]) - 0.5, d3.max(sourceData, d => d[xColumnName])])
+        .range([0, chartWidth]);
+
+    let xAxis = d3.axisBottom(xScale);
+
+
+    //- Prepare Y
+    let yScale = d3.scaleLinear()
+        .domain([d3.min(sourceData, d => d[yColumnName]) -0.5, d3.max(sourceData, d => d[yColumnName])])
+        .range([chartHeight, 0]);
+    
+    let yAxis = d3.axisLeft(yScale);
+
+
+    //- Create X-Y Axis
+    svgChartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(xAxis);
+
+    svgChartGroup.append("g")
+        .call(yAxis);
+
+
+    //- Create Points
+    let circleColor = d3.rgb(122, 182, 219)
+
+    let circleGroup = svgChartGroup.selectAll("circle")
+        .data(sourceData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(d[xColumnName]))
+        .attr("cy", d => yScale(d[yColumnName]))
+        .attr("r", "12")
+        .attr("fill", circleColor)
+        .attr("opacity", "0.9")
+
+
+    //- Create Labels
+    //  These labels are to be placed within own "g" otherwise the selectAll("text") picks up the 
+    //  axis text that results in a number of labels not being displaced
+    let svgLabelGroup = svgChartGroup.append("g");
+
+    var textMarkerGroup = svgLabelGroup.selectAll("text")
+        .data(sourceData)
+        .enter()
+        .append("text")
+        .attr("x", d => xScale(d[xColumnName]) - 6.8 )
+        .attr("y", d => yScale(d[yColumnName]) + 3.5)
+        .text(d => d.abbr)
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "10px")
+        .attr("fill", "white");
+
 }
 
 
@@ -129,6 +190,26 @@ d3.csv("assets/data/data.csv").then(function(chartData) {
     console.log("--> d3.csv: Request completed for census data");
 
     console.log(chartData);
+
+
+    //- Convert to numeric values
+    chartData.forEach(item => {
+        item.age = +item.age;
+        item.agoMoe = +item.agoMoe;
+        item.healthcare = +item.healthcare;
+        item.healthcareHigh = +item.healthcareHigh;
+        item.healthcareLow = +item.healthcareLow;
+        item.income = +item.income;
+        item.incomeMoe = +item.incomeMoe;
+        item.obesity = +item.obesity;
+        item.obesityHigh = +item.obesityHigh;
+        item.obesityLow = +item.obesityLow;
+        item.poverty = +item.poverty;
+        item.povertyMoe = +item.povertyMoe;
+        item.smokes = +item.smokes;
+        item.smokesHigh = +item.smokesHigh;
+        item.smokesLow = +item.smokesLow;
+    });
 
 
     //- Store data returned from endpoint in global variable; used with responsive design
