93d4210b36b2642cada7e79831cfb9cec08804f6
Scott Mac McEachern <the_fixer@hotmail.com>
2019-05-16 19:34:08 -0700

    Complete change in y axis

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,11 +16,24 @@ console.log("--> app.js: Started initialization of application");
 //- Prepare Globals
 const CHARTDIVNAME = "#scatter";
 
+const YAXISINACTIVECLASS = "aText inactive yAxis";  // class attribute for the SVG Y axis text elements that are not active
+
+const YAXISACTIVECLASS = "aText active yAxis";    // class attribute for the SVG Y axis text element that is active
+
+const XAXISINACTIVECLASS = "aText inactive xAxis";  // class attribute for the SVG X axis text elements that are not active
+
+const XAXISACTIVECLASS = "aText active xAxis";  // class attribute for the SVG X axis text element that is active
+
+const _chartMargin = {      // Margins to use to place the chart within the SVG container
+    top : 60,
+    right : 60,
+    bottom : 90,
+    left : 80
+};
+
 var _sourceChartData = null;    // census data from API used with charts; set when returned from API
 
 var _chartDivWidth = 0; // width of the div that contains the chart SVG; set when chart is created
-
-
 
 
 
@@ -35,6 +48,18 @@ function getChartDivWidth(){
     */
 
     return parseInt(d3.select("body").select(CHARTDIVNAME).style('width').slice(0, -2));
+}
+
+
+function getSvgHeight(){
+    /* Returns the height of the svg container; it is always calculated to be 50% of window height
+
+    Accepts : nothing
+
+    Returns : (number) height of the container in px
+    */
+
+    return (window.innerHeight * 0.5);
 }
 
 
@@ -69,20 +94,13 @@ function createChart(sourceData){
 
     //- Determine SVG Size
     //  There is other content to be on the page; use 50% of height
-    let svgHeight = (window.innerHeight * 0.5);
+    let svgHeight = getSvgHeight();
     _chartDivWidth = getChartDivWidth();
 
-    //- Set Chart Margins within SVG
-    let chartMargin = {
-        top : 60,
-        right : 60,
-        bottom : 60,
-        left : 60
-    };
 
     //- Set Chart Area; excludes the margins
-    let chartHeight = (svgHeight - chartMargin.top - chartMargin.bottom);
-    let chartWidth = (_chartDivWidth - chartMargin.left - chartMargin.right);
+    let chartHeight = (svgHeight - _chartMargin.top - _chartMargin.bottom);
+    let chartWidth = (_chartDivWidth - _chartMargin.left - _chartMargin.right);
 
     //- Create SVG Container
     let svgContainer = d3.select("body").select(CHARTDIVNAME).append("svg")
@@ -92,7 +110,7 @@ function createChart(sourceData){
     //- Create Chart Group within SVG
     //  Shift based on the margins
     let svgChartGroup = svgContainer.append("g")
-        .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+        .attr("transform", `translate(${_chartMargin.left}, ${_chartMargin.top})`);
 
 
     //- Prepare Tool-Tip
@@ -124,19 +142,17 @@ function createChart(sourceData){
     
     let yAxis = d3.axisLeft(yScale);
 
-
     //- Create X-Y Axis
     svgChartGroup.append("g")
         .attr("transform", `translate(0, ${chartHeight})`)
         .call(xAxis);
 
     svgChartGroup.append("g")
+        .attr("class", "yaxis")
         .call(yAxis);
 
 
     //- Create Points
-    let circleColor = d3.rgb(122, 182, 219)
-
     let circleGroup = svgChartGroup.selectAll("circle")
         .data(sourceData)
         .enter()
@@ -149,17 +165,6 @@ function createChart(sourceData){
         .on("mouseout", toolTip.hide);
 
 
-        // .attr("fill", circleColor)
-        // .attr("opacity", "0.9")
-
-        // .on("mouseover", function(d) {
-        //     d3.select(this).attr("fill", "red");
-        //     toolTip.show(this);
-        // })
-
-        // .on("mouseover", toolTip.show)
-
-
     //- Create Labels
     //  These labels are to be placed within own "g" otherwise the selectAll("text") picks up the 
     //  axis text that results in a number of labels not being displaced
@@ -169,41 +174,167 @@ function createChart(sourceData){
         .data(sourceData)
         .enter()
         .append("text")
-        .attr("x", d => xScale(d[chartInfo.xColumnName]) - 0 )
+        .attr("x", d => xScale(d[chartInfo.xColumnName]) )
         .attr("y", d => yScale(d[chartInfo.yColumnName]) + 4)
         .attr("class", "stateText")
         .text(d => d.abbr);
-        
-        // .attr("font-family", "sans-serif")
-        // .attr("font-size", "10px")
-        // .attr("fill", "white");
 
 
-    //- Create Axis Labels
+    //-- Create Axis Labels
     let svgAxisLabelGroup = svgChartGroup.append("g");
-    
+
+    //- Y: Lacks Healthcare
     svgAxisLabelGroup.append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - chartMargin.left + 35)
+        .attr("y", 0 - _chartMargin.left + 55)
         .attr("x", 0 - (chartHeight / 2))
-        .attr("class", "aText active")
-        .text("Lacks Healthcare(%)");
+        .attr("class", YAXISACTIVECLASS)
+        .attr("id", "healthcare")
+        .text("Lacks Healthcare(%)")
+        .on("click", updateYAxisChartData);
 
-
+    //- Y: Smokes
     svgAxisLabelGroup.append("text")
-        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top - 25})`)
-        .attr("class", "aText active")
-        .text("In Poverty (%)");
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - _chartMargin.left + 35)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("class", YAXISINACTIVECLASS)
+        .attr("id", "smokes")
+        .text("Smokes (%)")
+        .on("click", updateYAxisChartData);
+
+    //- Y: Obese
+    svgAxisLabelGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - _chartMargin.left + 15)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("class", YAXISINACTIVECLASS)
+        .attr("id", "obesity")
+        .text("Obese (%)")
+        .on("click", updateYAxisChartData);
+
+
+    //- X: In Poverty
+    svgAxisLabelGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + _chartMargin.top - 28})`)
+        .attr("class", XAXISACTIVECLASS)
+        .attr("id", "poverty")
+        .text("In Poverty (%)")
+        .on("click", updateXAxisChartData);
+
+    //- X: Age
+    svgAxisLabelGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + _chartMargin.top - 8})`)
+        .attr("class", XAXISINACTIVECLASS)
+        .attr("id", "age")
+        .text("Age (Median)")
+        .on("click", updateXAxisChartData);
+
+    //- X: Income
+    svgAxisLabelGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + _chartMargin.top +12})`)
+        .attr("class", XAXISINACTIVECLASS)
+        .attr("id", "income")
+        .text("Household Income (Median)")
+        .on("click", updateXAxisChartData);
+
+
 }
 
 
+function updateXAxisChartData(){
+    /*
 
-// chartGroup.selectAll("circle")
-//   .transition()
-//   .duration(1000)
-//   .attr("cx", (d, i) => xScale(i))
-//   .attr("cy", d => yScale(d));
+    */
 
+   console.log("--> updateXAxisChartData");
+
+
+   //-- Get Unique Identifer of data
+   let dataId = this.id;
+
+   console.log(`Unique identifier of data: ${dataId}`)
+
+
+    //-- Update Buttons
+
+    //- Set inactive class
+    d3.select(CHARTDIVNAME).selectAll(".xAxis").attr("class", XAXISINACTIVECLASS);
+
+    //- Make Active
+    d3.select(`#${dataId}`).attr("class", XAXISACTIVECLASS);
+
+
+}
+
+function updateYAxisChartData(){
+    /* Updates the chart when user clicks button; the "this" object contains the text element that the user
+    selected.  The ID of that element contains the unique identifier for the data to use.  Update the 
+    active/inactive state of the buttons and then updates the chart.
+
+    Accepts : nothing
+
+    Returns : undefined
+    */
+
+    console.log("--> updateYAxisChartData");
+
+
+    //-- Get Unique Identifer of data
+    let dataId = this.id;
+
+    console.log(`Unique identifier of data: ${dataId}`)
+
+
+    //-- Update Buttons
+
+    //- Set inactive class
+    d3.select(CHARTDIVNAME).selectAll(".yAxis").attr("class", YAXISINACTIVECLASS);
+
+    //- Make Active
+    d3.select(`#${dataId}`).attr("class", YAXISACTIVECLASS);
+
+
+
+    //- Update Y-Axis
+    let svgHeight = getSvgHeight();
+    let chartHeight = (svgHeight - _chartMargin.top - _chartMargin.bottom);
+
+    let yScale = d3.scaleLinear()
+        .domain([d3.min(_sourceChartData, d => d[dataId]) -0.5, d3.max(_sourceChartData, d => d[dataId])])
+        .range([chartHeight, 0]);
+
+    let yAxis = d3.axisLeft(yScale);
+
+
+    //- Update Chart: Y-Axis
+    let transitionDuration = d3.transition().duration(500);
+
+    d3.select(".yaxis")
+        .transition(transitionDuration)
+        .call(yAxis);
+
+
+    //- Update Chart: Circles
+    d3.selectAll("circle")
+        .transition(transitionDuration)
+        .attr("cy", d=> yScale(d[dataId]))
+    
+
+    //- Update Chart: Labels
+    d3.selectAll(".stateText")
+        .transition(transitionDuration)
+        .attr("y", d=> yScale(d[dataId]) + 4)
+    
+}
+
+
+// .on("mouseover", function(d) {
+        //     d3.select(this).attr("fill", "red");
+        //     toolTip.show(this);
+        // })
+
+        // .on("mouseover", toolTip.show)
 
 /*
 
